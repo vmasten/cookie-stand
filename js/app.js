d0aefb538c60fe6decce5ecd684d2fd2796c6b14
Vince Masten <vmasten@me.com>
2018-07-16 15:53:36 -0700

    corrected hourly sales behavior by rounding

--- js/app.js
+++ js/app.js
@@ -5,31 +5,32 @@ var firstAndPike = {
   maxCustomers: 65,
   cookieSalesAverage: 6.3,
   hourlyCustomers: function() {
-    return Math.floor(Math.random() * (this.minCustomers - this.maxCustomers + 1)) + this.minCustomers;
+    var min = Math.ceil(this.minCustomers);
+    var max = Math.floor(this.maxCustomers);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
 };
 
-var cookiesPerHour = function(cookies, customers) {
-  return cookies * customers;
+var cookiesPerHour = function(location) {
+  return Math.round(location.hourlyCustomers() * location.cookieSalesAverage);
 };
 
-var cookiesPerDay = function(hours) {
+var cookiesPerDay = function(hours, location) {
   var currentHour = 6;
   var arr = [];
   for (var i = 0; i < hours; i++) {
     if (currentHour < 12) {
-      arr.push(currentHour + ' am: ' + cookiesPerHour(firstAndPike.cookieSalesAverage, firstAndPike.hourlyCustomers()) + ' cookies');
+      arr.push(currentHour + ' am: ' + cookiesPerHour(location) + ' cookies');
     }
     else if (currentHour === 12) {
-      arr.push(currentHour + ' pm: ' + cookiesPerHour(firstAndPike.cookieSalesAverage, firstAndPike.hourlyCustomers()) + ' cookies');
+      arr.push(currentHour + ' pm: ' + cookiesPerHour(location) + ' cookies');
     } else {
-      arr.push((currentHour - 12) + ' pm: ' + cookiesPerHour(firstAndPike.cookieSalesAverage, firstAndPike.hourlyCustomers()) + ' cookies');
+      arr.push((currentHour - 12) + ' pm: ' + cookiesPerHour(location) + ' cookies');
     }
     currentHour++;
   }
   return arr;
 };
 
-console.log(cookiesPerHour(firstAndPike.cookieSalesAverage, firstAndPike.hourlyCustomers()));
-console.log(cookiesPerDay(14, cookiesPerHour(firstAndPike.cookieSalesAverage, firstAndPike.hourlyCustomers())));
+console.log(cookiesPerDay(15, firstAndPike));
