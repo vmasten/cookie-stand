4c26b0bd51109d7945ff9817003f152b91707afa
Vince Masten <vmasten@me.com>
2018-07-18 13:43:43 -0700

    refactoring hell

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 //Used later to build the table
+var allStores = [];
 var hours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'], openHours = 14, tblEl;
 
 function Stores(name, minCustomers, maxCustomers, salesAvg) {
@@ -8,32 +9,31 @@ function Stores(name, minCustomers, maxCustomers, salesAvg) {
   this.minCustomers = minCustomers;
   this.maxCustomers = maxCustomers;
   this.salesAvg = salesAvg;
+  this.customersPerHour = [];
+  this.cookiesPerHour = [];
+  this.dailyTotal = 0;
+
+  allStores.push(this);
 }
 
 //Simulates a random amount of customers per hour based on min and MaxCustomers
 Stores.prototype.hourlyCustomers = function() {
   var min = Math.ceil(this.minCustomers);
   var max = Math.floor(this.maxCustomers);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  this.customersPerHour.push(Math.floor(Math.random() * (max - min + 1)) + min);
 };
 
 
-//A simple function to get the cookies per hour; probably should be moved into each object
-Stores.prototype.cookiesPerHour = function() {
-  return Math.round(this.hourlyCustomers() * this.salesAvg);
+Stores.prototype.hourlySales = function() {
+  for (var hour of hours) {
+    this.cookiesPerHour.push(Math.round(this.hourlyCustomers() * this.salesAvg));
+  }
 };
 
 
 //Uses cookiesPerHour to build an array of sales by hour (including a total for the day)
-Stores.prototype.cookiesPerDay = function(hours) {
-  var arr = [], workingTotal = 0, total = 0;
-  for (var i = 0; i < hours; i++) {
-    workingTotal = this.cookiesPerHour();
-    arr.push(workingTotal);
-    total += workingTotal; //tracks the total cookies sold
-  }
-  arr.push(total);
-  return arr;
+Stores.prototype.cookiesPerDay = function() {
+  this.hourlySales();
 };
 
 //Uses the array built by cookiesPerDay to write the data to a table
@@ -48,8 +48,7 @@ Stores.prototype.render = function(hours) {
     tdEl.textContent = arr[idx];
     trEl.appendChild(tdEl);
   }
-  var sales = document.getElementById('locations');
-  sales.appendChild(trEl);
+  tblEl.appendChild(trEl);
 
 };
 
@@ -59,13 +58,12 @@ var tableSet = function() {
   tblEl = document.createElement('table');
   var h2El = document.createElement('h2');
   h2El.textContent = 'Cookies Needed By Location Each Day';
-  document.getElementById('locations').appendChild(h2El);
-
+  
   var trEl = document.createElement('tr');
   var blankEl = document.createElement('th');
   blankEl.textContent = '';
   trEl.appendChild(blankEl);
-
+  
   for (var idx in hours) {
     var thEl = document.createElement('th');
     thEl.textContent = hours[idx];
@@ -75,9 +73,9 @@ var tableSet = function() {
   total.textContent = 'Daily Location Total';
   trEl.appendChild(total);
 
-  var sales = document.getElementById('locations');
-  sales.appendChild(trEl);
-  sales.appendChild(tblEl);
+  tblEl.appendChild(trEl);
+  document.getElementById('locations').appendChild(h2El);
+  document.getElementById('locations').appendChild(tblEl);
 
 };
 
