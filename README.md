a86607144f726e1090a6541f1632f94c9c4e34f2
Vince Masten <vmasten@me.com>
2018-07-17 19:15:29 -0700

    finis

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Salmon Cookie Project
-A project to demonstrate proficiency with JavaScript objects, the DOM, and more.
+A project to demonstrate proficiency with JavaScript objects, the DOM, building tables, and more.
 
-Currently, it uses store objects to build unordered lists of cookies sold by hour and displays them in sales.html.
+Currently, it uses store objects to calculate how many cookies are sold per day and displays them in a table in sales.html.
--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var hours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'];
+//Used later to build the table
+var hours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'], openHours = 14;
 
 function Stores(name, minCustomers, maxCustomers, salesAvg) {
   this.name = name;
@@ -9,6 +10,7 @@ function Stores(name, minCustomers, maxCustomers, salesAvg) {
   this.salesAvg = salesAvg;
 }
 
+//Simulates a random amount of customers per hour based on min and MaxCustomers
 Stores.prototype.hourlyCustomers = function() {
   var min = Math.ceil(this.minCustomers);
   var max = Math.floor(this.maxCustomers);
@@ -22,30 +24,19 @@ Stores.prototype.cookiesPerHour = function() {
 };
 
 
-/* Uses cookiesPerHour to build an array of sales by hour (including a total for the day)
-and inserts the results into an unordered list in sales.html */
-
+//Uses cookiesPerHour to build an array of sales by hour (including a total for the day)
 Stores.prototype.cookiesPerDay = function(hours) {
-  var arr = [], workingTotal = 0, total = 0, currentHour = 6;
+  var arr = [], workingTotal = 0, total = 0;
   for (var i = 0; i < hours; i++) {
-    if (currentHour < 12) { //morning
-      workingTotal = this.cookiesPerHour();
-      arr.push(workingTotal);
-    }
-    else if (currentHour === 12) { //noon
-      workingTotal = this.cookiesPerHour();
-      arr.push(workingTotal);
-    } else { //evening
-      workingTotal = this.cookiesPerHour();
-      arr.push(workingTotal);
-    }
+    workingTotal = this.cookiesPerHour();
+    arr.push(workingTotal);
     total += workingTotal; //tracks the total cookies sold
-    currentHour++;
   }
   arr.push(total);
   return arr;
 };
 
+//Uses the array built by cookiesPerDay to write the data to a table
 Stores.prototype.render = function(hours) {
   var arr = this.cookiesPerDay(hours);
   var trEl = document.createElement('tr');
@@ -62,6 +53,8 @@ Stores.prototype.render = function(hours) {
 
 };
 
+//Sets up the first row of the table. Name was not intended to be cute
+//but couldn't bring myself to change it
 var tableSet = function() {
   var h2El = document.createElement('h2');
   h2El.textContent = 'Cookies Needed By Location Each Day';
@@ -77,24 +70,26 @@ var tableSet = function() {
   var total = document.createElement('th');
   total.textContent = 'Daily Location Total';
   trEl.appendChild(total);
-  
+
   var sales = document.getElementById('locations');
+  sales.appendChild(h2El);
   sales.appendChild(trEl);
 
 };
 
+//Object instantiation
 var pike = new Stores('1st and Pike', 23, 65, 6.3);
 var seaTac = new Stores('SeaTac Airport', 3, 24, 1.2);
 var seattleCenter = new Stores('Seattle Center', 11, 38, 3.7);
 var capHill = new Stores('Capitol Hill', 20, 38, 2.3);
 var alki = new Stores('Alki', 2, 16, 4.6);
 
+//Table building
 tableSet();
-pike.render(14);
-seaTac.render(14);
-seattleCenter.render(14);
-capHill.render(14);
-alki.render(14);
+pike.render(openHours);
+seaTac.render(openHours);
+seattleCenter.render(openHours);
+capHill.render(openHours);
+alki.render(openHours);
 
-/* I wasn't sure if some stores may have variable hours in the future, which is why I pass in
-hours to the function; additional work will be needed if stores have variable opening times */
+/* I wasn't sure if some stores may have variable hours in the future, which is why I (still) pass in hours to the function; additional work will be needed if stores have variable opening times */
